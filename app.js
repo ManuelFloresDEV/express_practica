f3098468bc3ee21702e2bb9f75e3d0da09b2d75e
Manuel Flores <speed_flores24@hotmail.com>
2024-07-27 17:33:52 -0600

    validation

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const fs = require("fs");
 
 const express = require("express");
 
+const port = 8080;
 const dataBase = "koders.json";
 const URLUsers = "/koders";
-const fileJson = "koders.json";
+
 const statusHTTP = {
   status204: [204, "no content found"],
   ststus201: [201, "added name"],
   status200: [200, "you have deleted from the database"],
+  status400: [400, "enter valid data"],
+  status404: [404, "not found"],
 };
-const app = express();
 
-const port = 8080;
 function init() {
   const exists = fs.existsSync(dataBase);
   if (!exists) {
@@ -21,21 +22,22 @@ function init() {
 }
 
 function getFileKoders() {
-  const data = fs.readFileSync(fileJson, "utf8");
+  const data = fs.readFileSync(dataBase, "utf8");
   const parseData = JSON.parse(data);
   return parseData;
 }
 
 function saveName(name) {
-  fs.writeFileSync(fileJson, JSON.stringify(name), "utf8");
+  fs.writeFileSync(dataBase, JSON.stringify(name), "utf8");
 }
 
+const app = express();
 init();
 
 app.get(URLUsers, (req, res) => {
   const listKoders = getFileKoders();
 
-  if (listKoders.names <= 0) {
+  if (!listKoders.names.length) {
     res.status(statusHTTP.status204[0]).end();
     return;
   }
@@ -45,8 +47,11 @@ app.post(`${URLUsers}/:name`, (req, res) => {
   const listKoders = getFileKoders();
   const nameKoder = req.params.name;
   listKoders.names.push(nameKoder);
-  console.log(nameKoder);
-  console.log(listKoders);
+
+  if (!nameKoder.trim() || !isNaN(nameKoder)) {
+    res.status(statusHTTP.status400[0]).end(statusHTTP.status400[1]);
+    return;
+  }
   saveName(listKoders);
   res
     .status(statusHTTP.ststus201[0])
@@ -55,6 +60,13 @@ app.post(`${URLUsers}/:name`, (req, res) => {
 app.delete(`${URLUsers}/:name`, (req, res) => {
   let listKoders = getFileKoders();
   const nameKoder = req.params.name;
+
+  if (!listKoders.names.includes(nameKoder)) {
+    res
+      .status(statusHTTP.status404[0])
+      .send(`${nameKoder}: ${statusHTTP.status404[1]} in the database `);
+    return;
+  }
   const newArray = listKoders.names.filter(
     (koder) => koder.toLowerCase() !== nameKoder.toLowerCase()
   );
@@ -67,9 +79,13 @@ app.delete(`${URLUsers}/:name`, (req, res) => {
 });
 app.delete(URLUsers, (req, res) => {
   let listKoders = getFileKoders();
+  if (!listKoders.names.length) {
+    res.status(statusHTTP.status404[0]).send("the database is empty");
+    return;
+  }
   listKoders.names = [];
-  saveName(listKoders);
   res.status(statusHTTP.status204[0]).end();
+  saveName(listKoders);
 });
 
 app.listen(port, () => {
